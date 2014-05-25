18cbcfd8ea5b5ab5e1c8fc743ff122cc8e7423a4
alexander <alexander.ressler@gmail.com>
2014-05-24 19:15:17 -0700

    linking tutorial driver

--- js/main.js
+++ js/main.js
@@ -4,8 +4,9 @@ define(
     "./rectangle",
     "./templates", 
     "./var/primeFactors",
-    "./var/appMessenger"
-  ], function(FTMessengerCentral, Rectangle, templates, primeFactors, appMessenger) {
+    "./var/appMessenger",
+    "./tutorial"
+  ], function(FTMessengerCentral, Rectangle, templates, primeFactors, appMessenger, Tutorial) {
 
     var containerID = "factor-trinomials";
     var app = {};
@@ -14,11 +15,32 @@ define(
     app.setup = setup;
     app.run = run;
 
+
+    function tutorialSetup() {
+        var tutorialMode = 0; // 0: off, 1: on
+        var tutorialStart = document.createElement("div");
+        var tutorial;
+        tutorialStart.onclick = function() {
+            if (!tutorialMode) {
+                tutorial = new Tutorial({ });
+                tutorial.insert();
+            }
+            else {
+                tutorial.remove();
+            }
+            tutorialMode = (tutorialMode) ? 0 : 1;
+            return null;
+        };
+        document.getElementById("factor-trinomials").appendChild(tutorialStart);
+        tutorialStart.setAttribute("class", "tutorial nav-button");
+        tutorialStart.innerHTML = "Tutorial";
+    }
+
+
     /** 
      * Prepare the DOM.
      */
     function setup() {
-
         // Fixes a bug in android browser: the touchmove event only fires once unless
         // preventDefault is called... lame...
         document.addEventListener("touchmove", function(event) {
@@ -34,7 +56,7 @@ define(
         var $rectangle  = $(templates.rectangle);
         var $diamond    = $(templates.diamond);
         var $numberPad  = $(templates.numberPad);
-        var body;
+        var body = document.querySelector("body");
 
         $con.append($trinomial); 
         $con.append($hints); 
@@ -47,12 +69,12 @@ define(
             height: window.innerHeight
         });
 
+        tutorialSetup();
 
         if (window.isMobile) {
             $(".numpad").css({ padding: "15px" });
         }
-
-        body = document.querySelector("body");
+       
 
         // events
         window.addEventListener("resize", function() {
@@ -64,7 +86,7 @@ define(
             });
         });
 
-        $("body").bind("touchmove", function(event) {
+        $(body).bind("touchmove", function(event) {
             event.preventDefault();
         });
 
